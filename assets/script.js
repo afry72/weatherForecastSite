d6283b44ea48dda4338c9c117c8d6f6f7344613d
afry72 <ajfrye72@gmail.com>
2024-05-01 16:54:26 -0400

    added more advanced html code with some css

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+//create searchbox function and save the output data to local storage
+
+//make a function that pulls 5 day forecast and displays into long forecast box
+
+// create function that puts weather info into temp hum and wind vars 
+
+//
