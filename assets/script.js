00d51d02d203d8c6c0aefb2af8ab2fc70b68bc34
afry72 <ajfrye72@gmail.com>
2024-05-07 01:35:06 -0400

    added weather api fetching code

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,70 @@
 //create searchbox function and save the output data to local storage
+$(document).ready(function() {
+    $(".searchBtn").on("click",function(event) {
+        event.preventDefault();
+        console.log("button pressed");
+        
+        var searchQuery = $('#location').val();
+        console.log(searchQuery);
+        
+        //save to local storage
 
-//make a function that pulls 5 day forecast and displays into long forecast box
+        fetchWeather(searchQuery);
+    });
 
-// create function that puts weather info into temp hum and wind vars 
+    function fetchWeather (searchQuery) {
+        console.log(searchQuery);
+    
+        if (searchQuery) {
+            // getting location from parameters
+          locFetch = 'http://api.openweathermap.org/geo/1.0/direct?q='+searchQuery+'&limit=1&appid=21e1eb8bb7f77cea96ca6673d97cf6cb';
+            
+        };
+    
+        console.log(searchQuery + "2");
+      
+        fetch(locFetch)
+          .then(function (response) {
+            if (!response.ok) {
+              throw response.json();
+            }
+      
+            return response.json();
+          })
+           .then(function (locRes) {
+            // write query to page so user knows what they are viewing
+            //resultTextEl.textContent = locRes.search.query; 
+      
+            console.log(locRes);
+            
+            var lat = locRes[0].lat;
 
-//
+            var lon = locRes[0].lon;
+
+            console.log(lat);
+            console.log(lon);
+        
+        if (lat && lon) {
+            locFetch = 'https://api.openweathermap.org/data/2.5/forecast?lat='+lat+'&lon='+lon+'&appid=21e1eb8bb7f77cea96ca6673d97cf6cb&units=imperial';
+        };
+
+        fetch(locFetch)
+          .then(function (response) {
+            if (!response.ok) {
+              throw response.json();
+            }
+      
+            return response.json();
+          })
+           .then(function (locRes) {
+      
+            console.log(locRes);
+            
+            })
+          });
+    };
+    
+    //fetchWeather();
+});
+
+
